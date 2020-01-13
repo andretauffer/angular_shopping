0d023c195b189dfb55916d2d9ede2a2c8d2a6101
andretauffer <andregiraotauffer@gmail.com>
2020-01-13 12:58:19 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./views/home/home.component";
+import { ProductsComponent } from "./views/products/products.component";
+import { CartComponent } from "./views/cart/cart.component";
+import { AboutComponent } from "./views/about/about.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "products", component: ProductsComponent },
+  { path: "cart", component: CartComponent },
+  { path: "about", component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { ProductsComponent } from './views/products/products.component';
+import { NavbarComponent } from './views/navbar/navbar.component';
+import { CartComponent } from './views/cart/cart.component';
+import { AboutComponent } from './views/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    NavbarComponent,
+    CartComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
